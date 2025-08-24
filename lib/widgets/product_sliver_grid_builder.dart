c6fcff34d89ef553b3dc1256bec59c779966dc89
Mohamed Hossam <m.mansi2hossam@gmail>
2025-08-24 14:55:35 +0300

    rafactoring UI

--- lib/widgets/product_sliver_grid_builder.dart
+++ lib/widgets/product_sliver_grid_builder.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store/cubit/get_product_cubit/get_product_cubit.dart';
+import 'package:store/cubit/get_product_cubit/get_product_state.dart';
 import 'package:store/widgets/sliver_grid_builder.dart';
 
-class ProductsSliverGridBuilder extends StatelessWidget {
+class ProductsSliverGridBuilder extends StatefulWidget {
   const ProductsSliverGridBuilder({super.key});
 
   @override
+  State<ProductsSliverGridBuilder> createState() =>
+      _ProductsSliverGridBuilderState();
+}
+
+class _ProductsSliverGridBuilderState extends State<ProductsSliverGridBuilder> {
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<GetProductCubit>(context).getAllProductsCubit();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SliverPadding(
-      padding: EdgeInsetsGeometry.symmetric(horizontal: 10, vertical: 30),
-      sliver: SliverGridBuilder(),
+    return SliverToBoxAdapter(
+      child: BlocBuilder<GetProductCubit, GetProductState>(
+        builder: (context, state) {
+          if (state is ProductSucess) {
+            final listOfProduct =
+                BlocProvider.of<GetProductCubit>(context).listProductCuibit ??
+                [];
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: GridViewBuilder(productModel: listOfProduct),
+            );
+          } else if (state is ProductFaliure) {
+            return Center(child: Text("Error: ${state.errMessage}"));
+          } else if (state is ProductLoading) {
+            return Center(
+              child: CircularProgressIndicator(color: Colors.black),
+            );
+          } else {
+            return Text('''We don't have any products yet''');
+          }
+        },
+      ),
     );
   }
 }
--- lib/widgets/sliver_grid_builder.dart
+++ lib/widgets/sliver_grid_builder.dart
@@ -1,91 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:store/cubit/get_product_cubit/get_product_cubit.dart';
-import 'package:store/cubit/get_product_cubit/get_product_state.dart';
+import 'package:store/models/product_model.dart';
 import 'package:store/widgets/product_cart.dart';
 
-class SliverGridBuilder extends StatefulWidget {
-  const SliverGridBuilder({super.key});
+class GridViewBuilder extends StatelessWidget {
+  const GridViewBuilder({super.key, required this.productModel});
 
-  @override
-  State<SliverGridBuilder> createState() => _SliverGridBuilderState();
-}
-
-class _SliverGridBuilderState extends State<SliverGridBuilder> {
-  @override
-  void initState() {
-    super.initState();
-    BlocProvider.of<GetProductCubit>(context).getAllProductsCubit();
-  }
+  final List<ProductModel> productModel;
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: BlocBuilder<GetProductCubit, GetProductState>(
-        builder: (context, state) {
-          if (state is ProductSucess) {
-            final listOfProduct =
-                BlocProvider.of<GetProductCubit>(context).listProductCuibit ??
-                [];
-            return GridView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return ProductCard(productModel: listOfProduct[index]);
-              },
-              itemCount: listOfProduct.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.2,
-                mainAxisSpacing: 40,
-                crossAxisSpacing: 10,
-              ),
-            );
-          } else if (state is ProductFaliure) {
-            return Center(child: Text("Error: ${state.errMessage}"));
-          } else if (state is ProductLoading) {
-            return Center(
-              child: CircularProgressIndicator(color: Colors.black),
-            );
-          } else {
-            return Text('''We don't have any products yet''');
-          }
-        },
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return ProductCard(productModel: productModel[index]);
+      },
+      itemCount: productModel.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1.2,
+        mainAxisSpacing: 40,
+        crossAxisSpacing: 10,
       ),
     );
   }
 }
-
-
-
-
-// FutureBuilder<List<ProductModel>>(
-//         future: GetAllProductService().getAllProducts(),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return Center(
-//               child: CircularProgressIndicator(color: Colors.black),
-//             );
-//           } else if (snapshot.hasError) {
-//             return Center(child: Text("Error: ${snapshot.error}"));
-//           } else if (snapshot.hasData) {
-//             final listOfProduct = snapshot.data!;
-//             return GridView.builder(
-//               physics: NeverScrollableScrollPhysics(),
-//               shrinkWrap: true,
-//               itemBuilder: (context, index) {
-//                 return ProductCard(productModel: listOfProduct[index]);
-//               },
-//               itemCount: listOfProduct.length,
-//               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                 crossAxisCount: 2,
-//                 childAspectRatio: 1.2,
-//                 mainAxisSpacing: 40,
-//                 crossAxisSpacing: 10,
-//               ),
-//             );
-//           } else {
-//             return Center(child: Text("No Data Found"));
-//           }
-//         },
-//       ),
