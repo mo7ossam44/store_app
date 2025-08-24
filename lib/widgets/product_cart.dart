5885a9331b9387aa2f378203e8258e612b6c3bf7
Mohamed Hossam <m.mansi2hossam@gmail>
2025-08-24 15:01:43 +0300

    add gesture detector to product Card

--- lib/widgets/product_cart.dart
+++ lib/widgets/product_cart.dart
@@ -15,57 +15,62 @@ class _ProductCardState extends State<ProductCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Card(
-          elevation: 5,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.productModel!.title.substring(0, 6),
-                  style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      r'$'
-                      '${widget.productModel!.price}',
-                      style: TextStyle(fontSize: 17),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: () {
-                        setState(() {
-                          isSelected = !isSelected;
-                        });
-                      },
-                      icon: Icon(
-                        Icons.favorite,
-                        color: isSelected ? Colors.red : Colors.grey.shade500,
+    return GestureDetector(
+      onTap: () {
+        
+      },
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.productModel!.title.substring(0, 11),
+                    style: TextStyle(color: Colors.grey.shade600, fontSize: 16),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        r'$'
+                        '${widget.productModel!.price}',
+                        style: TextStyle(fontSize: 17),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          setState(() {
+                            isSelected = !isSelected;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.favorite,
+                          color: isSelected ? Colors.red : Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        Positioned(
-          right: 32,
-          top: -20,
-          child: Image.network(
-            widget.productModel!.image,
-            height: 100,
-            width: 100,
+          Positioned(
+            right: 32,
+            top: -20,
+            child: Image.network(
+              widget.productModel!.image,
+              height: 100,
+              width: 100,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
